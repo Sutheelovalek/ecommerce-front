8bf334afe7a675fab46fd4bcfeadd2353bed6d71
suthee <lovalek@gmail.com>
2023-05-21 11:08:29 +0700

    feat: add remove and add product

--- pages/cart.js
+++ pages/cart.js
@@ -2,6 +2,7 @@ import Button from "@/components/Button";
 import { CartContext } from "@/components/CartContext";
 import Center from "@/components/Center";
 import Header from "@/components/Header";
+import Table from "@/components/Table";
 import axios from "axios";
 import { useContext, useEffect, useState } from "react";
 import styled from "styled-components";
@@ -17,9 +18,31 @@ const Box = styled.div`
   border-radius: 10px;
   padding: 30px;
 `;
+const ProductInfoCell = styled.td`
+  padding: 10px 0;
+
+`;
+const ProductImageBox = styled.div`
+  width: 100px;
+  height: 100px;
+  padding: 10px;
+  border-radius: 10px;
+  border-top: 1px solid rgba(0, 0, 0, 0.1);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  img{
+    max-width: 80px;
+    max-height: 80px;
+  }
+`;
+const QuantityLabel = styled.span`
+  padding: 0 3px;
+`;
+
 
 export default function CartPage() {
-  const { cartProducts } = useContext(CartContext);
+  const { cartProducts, addProduct, removeProduct } = useContext(CartContext);
   const [products, setProducts] = useState([]);
   useEffect(() => {
     if (cartProducts.length > 0) {
@@ -28,6 +51,12 @@ export default function CartPage() {
       });
     }
   }, [cartProducts]);
+  function moreOfThisProduct(id) {
+    addProduct(id);
+  }
+  function lessOfThisProduct(id) {
+    removeProduct(id);
+  }
   return (
     <>
       <Header />
@@ -35,29 +64,51 @@ export default function CartPage() {
         <ColumnsWrapper>
           <Box>
             <h2>Cart</h2>
-            {!cartProducts?.length && <div>Your cart is empty</div>}
-            {products?.length > 0 && (
-              <table>
-                <thead>
-                  <tr>
-                    <th>Product</th>
-                    <th>Quantity</th>
-                    <th>Price</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {products.map((product) => (
-                    <tr key={product._id}>
-                      <td> {product.title}</td>
-                      <td>
-                        {cartProducts.filter((id) => id === product._id).length}
-                      </td>
-                      <td>price</td>
-                    </tr>
-                  ))}
-                </tbody>
-              </table>
-            )}
+            {!cartProducts?.length && (
+            <div>Your cart is empty</div>)}
+           {products?.length > 0 && (
+
+  <Table>
+    <thead>
+      <tr>
+        <th>Product</th>
+        <th>Quantity</th>
+        <th>Price</th>
+      </tr>
+    </thead>
+    <tbody>
+      {products.map((product) => (
+        <tr key={product._id}>
+          <ProductInfoCell>
+            <ProductImageBox>
+            <picture>
+           <img src={product.images[0]} alt='product'/> 
+           </picture>
+            </ProductImageBox>
+                {product.title}
+          </ProductInfoCell>
+          <td>
+            <Button onClick={() => lessOfThisProduct(product._id)}>
+              -
+            </Button>
+            <QuantityLabel>
+             {cartProducts.filter((id) => id === product._id).length}
+            </QuantityLabel>
+            <Button onClick={() => moreOfThisProduct(product._id)}>
+              +
+            </Button>
+          </td>
+          <td>
+            ฿{(
+              cartProducts.filter((id) => id === product._id).length *
+              product.price
+              ).toLocaleString()}
+          </td>
+        </tr>
+      ))}
+    </tbody>
+  </Table>
+)}
           </Box>
           {!!cartProducts?.length && (
             <Box>
